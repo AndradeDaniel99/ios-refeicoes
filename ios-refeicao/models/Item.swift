5fecd0f9912c1a33dae19d5660dd17394578348c
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-05-12 16:29:08 -0300

    criando nscoding na classe item

--- ios-refeicao/models/Item.swift
+++ ios-refeicao/models/Item.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class Item: NSObject {
+class Item: NSObject, NSCoding {
+
+    // MARK: - Atributos
 
     let nome: String
     let calorias: Double
+    
+    
+    // MARK: - NSCoding
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(nome, forKey: "nome")
+        coder.encode(calorias, forKey: "calorias")
+    }
+    
+    required init?(coder: NSCoder) {
+        <#code#>
+    }
+    
 
+    // MARK: - Init
+    
     init(nome: String, calorias: Double) {
         self.nome = nome
         self.calorias = calorias
