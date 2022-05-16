3681ff0352ada7ffed46a8a66b5f99f967785748
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-05-16 16:07:38 -0300

    refatorando o codigo

--- ios-refeicao/models/RefeicaoDao.swift
+++ ios-refeicao/models/RefeicaoDao.swift
@@ -26,5 +26,11 @@ class RefeicaoDao {
         
     }
     
+    func recuperaCaminho() -> URL? {
+        guard let diretorio = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        let caminho = diretorio.appendingPathComponent("refeicao")
+        return caminho
+    }
+    
     
 }
