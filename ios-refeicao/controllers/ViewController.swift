1645e8d253e88a2392f75300ddfba14b491e1097
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2022-05-16 19:58:20 -0300

    refatorando o codigo

--- ios-refeicao/controllers/ViewController.swift
+++ ios-refeicao/controllers/ViewController.swift
@@ -78,23 +78,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             Alerta(controller: self).exibe()
         }
         
-        do {
-            let dados = try NSKeyedArchiver.archivedData(withRootObject: itens, requiringSecureCoding: false)
-            guard let caminho = recuperaDiretorio() else { return }
-            try dados.write(to: caminho)
-        } catch {
-            print(error.localizedDescription)
-        }
-        
-        
-        
     }
     
-    func recuperaDiretorio()-> URL? {
-        guard let diretorio = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-        let caminho = diretorio.appendingPathComponent("itens")
-        return caminho
-    }
+    
     
     
     // MARK: - UITableViewDataSource
